2b27a89c2903f1c5ab1671344dc1e10b62a9f83f
Jason Agostoni <jagostoni@ceiamerica.com>
2016-02-23 10:59:13 -0500

    Initial skeleton API project

---
+++ Band.WebAPI/Controllers/ButtonCountsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Band.WebAPI.Controllers
+{
+    public class ButtonCountsController : ApiController
+    {
+
+
+        public static ButtonData _DATA = new ButtonData() { Blue = 5, Green = 4, Red = 3, Yellow = 2};
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            _DATA.Yellow++;
+            return Ok(_DATA);
+
+        }
+    }
+
+
+    public class ButtonData
+    {
+        public long Red { get; set; }
+        public long Green { get; set; }
+        public long Blue { get; set; }
+        public long Yellow { get; set; }
+    }
+}
---
+++ Band.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Band.WebAPI.MvcApplication" Language="C#" %>
