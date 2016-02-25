4970d6a7f77d790822c33d38352dc54e85cbbab9
Geoff Tewksbury <geoff.tewksbury@gmail.com>
2016-02-24 22:26:56 -0500

    moved connection string to config file

--- WebJob/Functions.cs
+++ WebJob/Functions.cs
@@ -8,26 +8,21 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.ServiceBus.Messaging;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Configuration;
 
 namespace WebJob
 {
     public class Functions
     {
-        static string CONNECTION_STRING = "<set connection string>";
         static string IOTHUB_ENDPOINT = "messages/events";
-        static EventHubClient EVENTHUB_CLIENT;
         static string WEBAPI_ADDRESS = "http://buttonband.azurewebsites.net";
 
         [NoAutomaticTrigger]
         public static async Task ProcessEvents(TextWriter log)
         {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(WEBAPI_ADDRESS);
-                await client.PostAsJsonAsync<string>("api/ButtonCounts", "red");
-            }
+            var connectionString = ConfigurationManager.ConnectionStrings["IoTHubConnectionString"].ToString();
 
-            var eventHubClient = EventHubClient.CreateFromConnectionString(CONNECTION_STRING,IOTHUB_ENDPOINT);
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString,IOTHUB_ENDPOINT);
             var partitions = eventHubClient.GetRuntimeInformation().PartitionIds;
 
             foreach(string partition in partitions)
@@ -47,9 +42,11 @@ namespace WebJob
 
                 string data = Encoding.UTF8.GetString(eventData.GetBytes());
 
-
-
-                Console.WriteLine(string.Format("Message received. Partition: {0} Data: '{1}'", partition, data));
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(WEBAPI_ADDRESS);
+                    await client.PostAsJsonAsync<string>("api/ButtonCounts", data);
+                }
             }
         }
     }
